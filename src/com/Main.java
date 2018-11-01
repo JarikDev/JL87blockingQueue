4a82ca9c64287a410287a7f3e5639a9693e0ffe2
ivan4000 <sxln1@mail.ru>
2018-11-01 17:49:11 +0300

    JL87blickingQueue

---
+++ src/com/Main.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new PriorityBlockingQueue<>();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println(queue.add("this is string"));
+            }
+        }.start();
+    }
+}
